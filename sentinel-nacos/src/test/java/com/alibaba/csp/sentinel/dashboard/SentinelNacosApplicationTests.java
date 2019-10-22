77aaf6455000b274eccc427eaa54ef6cd9abf5bf
lipeng <lp960419@163.com>
2019-10-22 19:55:06 +0800

    提交

---
+++ sentinel-nacos/src/test/java/com/alibaba/csp/sentinel/dashboard/SentinelNacosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibaba.csp.sentinel.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelNacosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
